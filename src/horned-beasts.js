5d1d7558cebf50111483c8b851992bc243fb0a30
yazantafesh <yazantafesh1@gmail.com>
2021-05-17 16:49:37 +0300

    test3

--- src/horned-beasts.js
+++ src/horned-beasts.js
@@ -3,41 +3,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-class HornedBeasts extends React.Component{
-  
-  constructor(props){
+class HornedBeasts extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      votes:0,
+      votes: 0,
     };
-  }  
-
-  increaseVotes = () =>{
+  }
+  increaseVotes = () => {
     this.setState({
       votes: this.state.votes + 1,
     });
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
-
-        {/* <h2>{this.props.title}</h2>
-        <img onClick={this.increaseVotes} src={this.props.imageUrl} alt={this.props.title} title={this.props.title}></img>
-        <p>{this.props.description}</p>
-        <p>❤️: {this.state.votes}</p> */}
-
-        <Card style={{ width: '18rem'}}>
-          <Card.Img  variant="top" src={this.props.imageUrl} />
+        <Card style={{ width: '18rem' }} key={this.props}>
+          <Card.Img variant="top" src={this.props.imageUrl} />
           <Card.Body>
             <Card.Title>{this.props.title}</Card.Title>
             <Card.Text>
-            ❤️: {this.state.votes}
+              ❤️: {this.state.votes}
             </Card.Text>
           </Card.Body>
           <Button onClick={this.increaseVotes} variant="primary">Vote</Button>
         </Card>
-
       </div>
     );
   }
