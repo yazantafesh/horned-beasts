8a9d33ac7769d24d695b6b726a9521500220d6cb
yazantafesh <yazantafesh1@gmail.com>
2021-05-18 17:54:51 +0300

    done lab 3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React from 'react';
-import Footer from './footer';
-import Header from './header';
-import Main from './main';
+import Footer from './components/footer';
+import Header from './components/header';
+import Main from './components/main';
+import HornsData from './components/data.json';
+import SelectedBeast from './components/SelectedBeast';
 
 class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      HornsData:this.props.HornsData,
+      savedElement:{},
+      display: false,
+    };
+  }
+
+  handleClose = () => {
+    this.setState({
+      display: false,
+    });
+  }
+
+  handleShow = (param) =>{
+    let result = HornsData.find(element => {
+      if (element.title === param) {
+        return element;
+      }
+    });
+    this.setState({
+      savedElement: result,
+      display: true,
+    });
+  }
 
   render(){
 
     return(
       <div>
         <Header/>
-        <Main/>
+        <Main HornsData = {HornsData} handleShow = {this.handleShow}/>
+        <SelectedBeast display={this.state.display} handleClose = {this.handleClose} element = {this.state.savedElement}/>
         <Footer/>
       </div>
     );
